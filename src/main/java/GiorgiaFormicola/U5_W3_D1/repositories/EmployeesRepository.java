289e783c2bcab81ffdb5fda31cf4951a57dd4f98
GiorgiaFormicola <giorgia.formicola97@gmail.com>
2026-04-20 14:58:59 +0200

    Adds findByEmail() EmployeesRepository

--- src/main/java/GiorgiaFormicola/U5_W3_D1/repositories/EmployeesRepository.java
+++ src/main/java/GiorgiaFormicola/U5_W3_D1/repositories/EmployeesRepository.java
@@ -4,6 +4,7 @@ import GiorgiaFormicola.U5_W3_D1.entities.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
@@ -11,4 +12,6 @@ public interface EmployeesRepository extends JpaRepository<Employee, UUID> {
     boolean existsByEmail(String email);
 
     boolean existsByUsername(String username);
+
+    Optional<Employee> findByEmail(String email);
 }
